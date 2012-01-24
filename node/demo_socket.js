accfe65b97bb1c6bca1530bb2681feb962a0b64c
Pedro Burón <pedro@witoi.com>
2012-01-23 21:21:07 -0300

    socket for new functions

--- node/demo_socket.js
+++ node/demo_socket.js
@@ -2,10 +2,10 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(80);
+app.listen(3000);
 
 function handler (req, res) {
-  var peticion = (req.url == '/') ? '/index.html' : req.url;
+  var peticion = (req.url == '/') ? '/pizarra.html' : req.url;
   fs.readFile(__dirname + peticion,
   function (err, data) {
     if (err) {
@@ -18,43 +18,15 @@ function handler (req, res) {
   });
 }
 
-/*var conexiones = {servidor:'', cliente:''}
-io.sockets.on('connection', function (socket) {
-	// socket.emit('news', { hello: 'world' });
-	socket.on('iniciar', function(data) {
-		if(data.soy == 'servidor') {
-			conexiones.servidor = socket;
-			console.log('tenemos servidor!');
-			//console.log(conexiones)
-		}else{
-			conexiones.cliente = socket;
-			console.log('tenemos cliente!');
-			//console.log(conexiones)
-		}
-	});
-	socket.on('mouse', function (data) {
-		conexiones.cliente.emit('news', data);
-	});
-});*/
-var clickX = new Array();
-var clickY = new Array();
-var clickDrag = new Array();
 var conexiones = [];
+var points = {}
 io.sockets.on('connection', function (socket) {
 	conexiones.push(socket);
 	console.log('conexion registrada');
 	
-	socket.on('obtenerPizarra', function(data) {
-		socket.emit('datosPizarra', pizarra);
-	});
-	
+	socket.emit('redraw', points);
 	socket.on('addClick', function(data) {
-		clickX.push(data.x);
-		clickY.push(data.y);
-		clickDrag.push(data.dragging);
-		for(var i = 0; i<conexiones.length; i++) {
-			var cliente  = conexiones[i];
-			cliente.emit('redraw', {x: clickX, y: clickY, drag: clickDrag})
-		}
+		points[data.x + 'x' + data.y] = data.color;
+		socket.emit('redraw', points);
 	})
 });
